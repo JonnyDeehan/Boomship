b5710dced90e084d6fe34def70044fe03574cef6
Jonathan Deehan <jonathandeehan@Jonathans-MacBook-Pro.local>
2017-01-05 12:18:26 +0000

    Still In Development - working between mac and windows

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float speed;
+	private float minX, minY, maxX, maxY;
+	private GameObject gameMasterObject;
+	private GameMaster gameMaster;
+
+	void Awake (){
+		gameMasterObject = GameObject.FindGameObjectWithTag ("GameMaster");
+		gameMaster = gameMasterObject.GetComponent<GameMaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		Move (direction);
+	}
+
+	void Move(Vector2 direction){
+
+		minX = gameMaster.minX;
+		minY = gameMaster.minY;
+		maxX = gameMaster.maxX;
+		maxY = gameMaster.maxY;
+
+		Vector2 pos = transform.position;
+
+		pos += direction * speed * Time.deltaTime;
+
+		pos.x = Mathf.Clamp (pos.x, minX, maxX);
+		pos.y = Mathf.Clamp (pos.y, minY, maxY);
+
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	public float speed;
+	private GameMaster gameMaster;
+
+	void Awake(){
+		gameMaster = GameObject.FindGameObjectWithTag ("GameMaster").GetComponent<GameMaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 position = transform.position;
+
+		position = new Vector2 (position.x, position.y + speed * Time.deltaTime);
+
+		transform.position = position;
+
+		if (transform.position.y < gameMaster.minY) {
+			transform.position = new Vector2 (Random.Range (gameMaster.minX, gameMaster.maxX), gameMaster.maxY);
+		}
+	
+	}
+}
